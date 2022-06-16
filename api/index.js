f807897d782e1026f7ebe97f8451ffbbc8a6c823
Pablo Causa <causapablo@gmail.com>
2022-06-16 15:56:12 -0300

    Froce true

--- api/index.js
+++ api/index.js
@@ -24,7 +24,7 @@ const {apiGetAll} = require("./src/controllers/recipe/apiStringRequest");
 const {API_KEY } = process.env;
 
 // Syncing all the models at once.
-conn.sync({ force: false }).then(() => {
+conn.sync({ force: true }).then(() => {
   server.listen(process.env.PORT, () => {
     console.log('%s listening at 3001');
     // eslint-disable-line no-console
